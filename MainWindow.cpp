952633c0a990a792e1ccc7bd1e62726a649711ea
姜凯 <kai.jiang@nidone.com.cn>
2019-03-27 18:15:29 +0800

    实现左右滑动效果，但是无法鼠标拖动，只能鼠标点击

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -5,9 +5,12 @@
 MainWindow::MainWindow(QWidget *parent) :
 	QMainWindow(parent),
 	ui(new Ui::MainWindow),
-	m_index(0)
+	m_index(0), m_flag(false), m_direction(LEFT)
 {
 	ui->setupUi(this);
+
+	m_imageLabel = new QLabel(ui->pictureWidget);
+	m_imageLabel->setGeometry(0, 0, 420, 300);
 
 	Q_INIT_RESOURCE(pictures);
 
@@ -17,16 +20,27 @@ MainWindow::MainWindow(QWidget *parent) :
 
 	for (int i = 0; i < 3; i ++)
 	{
+		/*
 		QString path = QString(":/pictures/%1.png").arg(i+1);
 		qDebug() << "path: " << path;
 		QImage *image = new QImage(path);
 		m_imageList.append(image);
+		*/
+
+		QLabel *label = new QLabel(ui->pictureWidget);
+		m_imageLabel->setGeometry(0, 0, 420, 300);
+		QImage image(QString(":/pictures/%1.png").arg(i + 1));
+		label->setPixmap(QPixmap::fromImage(image));
+
+		m_labelList.append(label);
 	}
 
-	showPicture(m_imageList.first());
+	showPicture(m_labelList.first());
 
 	connect(ui->leftButton, SIGNAL(clicked(bool)), this, SLOT(prev()));
 	connect(ui->rigthButton, SIGNAL(clicked(bool)), this, SLOT(next()));
+
+	connect(&m_timer, SIGNAL(timeout()), this, SLOT(movePicture()));
 }
 
 MainWindow::~MainWindow()
@@ -34,25 +48,86 @@ MainWindow::~MainWindow()
 	delete ui;
 }
 
-void MainWindow::showPicture(QImage *image)
+void MainWindow::showPicture(QLabel *image)
 {
-	ui->imageLabel->setPixmap(QPixmap::fromImage(*image));
+	for (int i = 0; i < m_labelList.size(); i++)
+	{
+		if (image == m_labelList.at(i))
+			image->move(0, 0);
+		else
+			m_labelList.at(i)->move(420, 0);
+	}
 }
 
 void MainWindow::prev()
 {
+	if (m_flag)
+		return;
+
+	m_flag = true;
+	m_direction = LEFT;
 	if (0 == m_index)
 		m_index = 2;
 	else
 		m_index --;
 
-	showPicture(m_imageList.at(m_index));
+//	showPicture(m_imageList.at(m_index));
+//	m_imageLabel->move(0, 0);
+
+	m_timer.start(20);
 }
 
 void MainWindow::next()
 {
+	if (m_flag)
+		return;
+
+	m_flag = true;
+	m_direction = RIGHT;
 	m_index ++;
 	m_index %= 3;
 
-	showPicture(m_imageList.at(m_index));
+//	showPicture(m_imageList.at(m_index));
+
+	m_timer.start(20);
+}
+
+void MainWindow::movePicture()
+{
+	QLabel *prevLabel;
+	if (RIGHT == m_direction)
+		prevLabel = m_labelList.at(m_index > 0 ? (m_index - 1) : 2);
+	else
+		prevLabel = m_labelList.at((m_index == 2) ? 0 : (m_index + 1));
+
+	int prevLabelX = prevLabel->x();
+	QLabel *curLabel = m_labelList.at(m_index);
+
+	if (RIGHT == m_direction)
+	{
+		if (prevLabelX < 420)
+		{
+			prevLabelX += 10;
+			int curLabelX = -420 + prevLabelX;
+			prevLabel->move(prevLabelX, 0);
+			curLabel->move(curLabelX, 0);
+		} else
+		{
+			m_timer.stop();
+			m_flag = false;
+		}
+	} else
+	{
+		if (prevLabelX > -420)
+		{
+			prevLabelX -= 10;
+			int curLabelX = 420 - (-prevLabelX);
+			prevLabel->move(prevLabelX, 0);
+			curLabel->move(curLabelX, 0);
+		} else
+		{
+			m_timer.stop();
+			m_flag = false;
+		}
+	}
 }
--- MainWindow.h
+++ MainWindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 #include <QList>
+#include <QLabel>
+#include <QTimer>
 
 namespace Ui {
 class MainWindow;
@@ -12,21 +14,32 @@ class MainWindow : public QMainWindow
 {
 	Q_OBJECT
 
+	enum _DIRECTION {
+		LEFT,
+		RIGHT
+	};
+
 public:
 	explicit MainWindow(QWidget *parent = 0);
 	~MainWindow();
 
-	void					showPicture(QImage *image);
+	void					showPicture(QLabel *image);
 
 public slots:
 	void					prev();
 	void					next();
 
+	void					movePicture();
+
 private:
 	Ui::MainWindow *ui;
 
+	QLabel					*m_imageLabel;
 	quint32					m_index;
-	QList<QImage *>			m_imageList;
+	QList<QLabel *>			m_labelList;
+	QTimer					m_timer;
+	bool					m_flag;
+	_DIRECTION				m_direction;
 };
 
 #endif // MAINWINDOW_H
